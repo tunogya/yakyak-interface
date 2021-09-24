226a25460a65053f8867941adc55326a2528b151
tunogya <tunogya@qq.com>
2021-09-24 13:34:29 +0800

    add Header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,33 @@
-import { Grid } from '@chakra-ui/react'
+import {Button, Grid, Stack, Text} from '@chakra-ui/react'
+import {useHistory} from "react-router-dom";
+import {useState} from "react";
 
 export const Header = () => {
+  const links = [
+    {path: "/wallet", label: "Wallet"},
+    {path: "/swap", label: "Swap"},
+  ]
+  const history = useHistory()
+  const [currentPath, setCurrentPath] = useState(history.location.pathname)
+
   return (
-    <Grid t={0} p={6}>
-      Create React Dapp
+    <Grid templateColumns="repeat(3, 1fr)" p={4} gap={6} alignItems={"center"}>
+      <Stack justifySelf={"flex-start"}>
+        <Text>Create React Dapp</Text>
+      </Stack>
+      <Stack justifySelf={"center"} direction={"row"} background={"white"} p={1} borderRadius={"md"}>
+        {links.map((link, index) => (
+          <Button key={index} colorScheme={"gray"} size={"sm"} variant={currentPath === link.path ? "solid" : "ghost"}
+                  onClick={() => {
+                    history.push(link.path)
+                    setCurrentPath(link.path)
+                  }}>{link.label}</Button>
+        ))}
+      </Stack>
+      <Stack justifySelf={"flex-end"} direction={"row"} alignItems={"center"}>
+        <Button variant={"outline"} colorScheme={"black"} size={"sm"}>Address</Button>
+        <Button size={"sm"}>Menu</Button>
+      </Stack>
     </Grid>
   )
 }

022b292a318c8f4405fa2378a0f9a2930c5514ec
tunogya <tunogya@qq.com>
2021-09-24 14:34:19 +0800

    add wallet

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -1,6 +1,24 @@
+import {Button, FormControl, FormHelperText, FormLabel, Input, Spacer, Stack} from "@chakra-ui/react";
+
 export const Wallet = () => {
   return (
-    <>Wallet</>
+    <Stack w={"600px"} h={"600px"} p={6} borderRadius={"xl"} spacing={6}>
+      <FormControl id="To">
+        <FormLabel>From</FormLabel>
+        <Input variant="filled" disabled/>
+      </FormControl>
+      <FormControl id="To">
+        <FormLabel>To</FormLabel>
+        <Input variant="filled"/>
+      </FormControl>
+      <FormControl id="Amount">
+        <FormLabel>Amount</FormLabel>
+        <Input type={"number"} variant="filled"/>
+        <FormHelperText>My balance: 0</FormHelperText>
+      </FormControl>
+      <Spacer/>
+      <Button colorScheme={"blue"}>Transfer</Button>
+    </Stack>
   )
 }
 

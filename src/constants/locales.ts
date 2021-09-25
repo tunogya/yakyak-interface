531eb8f1f9fd4ed735ed30a0667b16b22cde36ee
tunogya <tunogya@qq.com>
2021-09-25 17:49:19 +0800

    set default en-US

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,11 +1,10 @@
 export const SUPPORTED_LOCALES = [
-  // order as they appear in the language dropdown
   'en-US',
   'zh-CN',
 ] as const
 export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
 
-export const DEFAULT_LOCALE: SupportedLocale = 'zh-CN'
+export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
 
 export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
   'en-US': 'English',

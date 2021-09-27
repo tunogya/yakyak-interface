efcd67e8219e78f667b40b15634154f975178dc8
tunogya <tunogya@qq.com>
2021-09-27 18:39:09 +0800

    delete language title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -61,7 +61,7 @@ export const Header = () => {
               {colorMode === "light" ? "Dark" : "Light"} Mode
             </MenuItem>
             <MenuDivider/>
-            <MenuOptionGroup defaultValue={locale} title="language" type="radio">
+            <MenuOptionGroup defaultValue={locale} type="radio">
 
               {SUPPORTED_LOCALES.map((locale, index) => (
                 <MenuItemOption value={locale} key={index}

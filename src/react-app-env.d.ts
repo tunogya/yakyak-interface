8da26b3f370bca28173c4b3d562d7113f07757fa
tunogya <tunogya@qq.com>
2021-09-28 11:05:02 +0800

    add formatic

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,10 @@
 /// <reference types="react-scripts" />
 
-declare module "@metamask/jazzicon" {
+declare module '@metamask/jazzicon' {
   export default function (diameter: number, seed: number): HTMLElement
 }
+
+declare module 'fortmatic'
 
 interface Window {
   ethereum?: {
@@ -14,12 +16,12 @@ interface Window {
   web3?: Record<string, unknown>
 }
 
-declare module "content-hash" {
+declare module 'content-hash' {
   declare function decode(x: string): string
   declare function getCodec(x: string): string
 }
 
-declare module "multihashes" {
+declare module 'multihashes' {
   declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
   declare function toB58String(hash: Uint8Array): string
 }

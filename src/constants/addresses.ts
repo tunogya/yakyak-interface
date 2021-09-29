1913eb26ef7303f4411a643e9511369b99de283f
tunogya <tunogya@qq.com>
2021-09-29 16:21:17 +0800

    add abis

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -9,3 +9,11 @@ export const NEST_ADDRESS: AddressMap = {
   [SupportedChainId.MAINNET]: '0x04abeda201850ac0124161f037efd70c74ddc74c',
   [SupportedChainId.RINKEBY]: '0x8d6b97c482ecc00d83979dac4a703dbff04fd84f',
 }
+
+export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: AddressMap = constructSameAddressMap(
+  '0xC36442b4a4522E871399CD717aBDD847Ab11FE88'
+)
+
+export const ARGENT_WALLET_DETECTOR_ADDRESS: AddressMap = {
+  [SupportedChainId.MAINNET]: '0xeca4B0bDBf7c55E9b7925919d03CbF8Dc82537E8',
+}
---
+++ src/hooks/useContract.ts
@@ -0,0 +1,59 @@
+import { Contract } from '@ethersproject/contracts'
+import ARGENT_WALLET_DETECTOR_ABI from '../abis/argent-wallet-detector.json'
+import ERC20_ABI from '../abis/erc20.json'
+import ERC20_BYTES32_ABI from '../abis/erc20_bytes32.json'
+import WETH_ABI from '../abis/weth.json'
+import EIP_2612 from '../abis/eip_2612.json'
+
+import {
+  ARGENT_WALLET_DETECTOR_ADDRESS,
+} from '../constants/addresses'
+import { useMemo } from 'react'
+import { getContract } from '../utils'
+import { Erc20, ArgentWalletDetector, Weth } from '../abis/types'
+import { WETH9_EXTENDED } from '../constants/tokens'
+import { useActiveWeb3React } from './web3'
+
+// returns null on errors
+export function useContract<T extends Contract = Contract>(
+  addressOrAddressMap: string | { [chainId: number]: string } | undefined,
+  ABI: any,
+  withSignerIfPossible = true
+): T | null {
+  const { library, account, chainId } = useActiveWeb3React()
+
+  return useMemo(() => {
+    if (!addressOrAddressMap || !ABI || !library || !chainId) return null
+    let address: string | undefined
+    if (typeof addressOrAddressMap === 'string') address = addressOrAddressMap
+    else address = addressOrAddressMap[chainId]
+    if (!address) return null
+    try {
+      return getContract(address, ABI, library, withSignerIfPossible && account ? account : undefined)
+    } catch (error) {
+      console.error('Failed to get contract', error)
+      return null
+    }
+  }, [addressOrAddressMap, ABI, library, chainId, withSignerIfPossible, account]) as T
+}
+
+export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: boolean) {
+  return useContract<Erc20>(tokenAddress, ERC20_ABI, withSignerIfPossible)
+}
+
+export function useWETHContract(withSignerIfPossible?: boolean) {
+  const { chainId } = useActiveWeb3React()
+  return useContract<Weth>(chainId ? WETH9_EXTENDED[chainId]?.address : undefined, WETH_ABI, withSignerIfPossible)
+}
+
+export function useArgentWalletDetectorContract() {
+  return useContract<ArgentWalletDetector>(ARGENT_WALLET_DETECTOR_ADDRESS, ARGENT_WALLET_DETECTOR_ABI, false)
+}
+
+export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(tokenAddress, ERC20_BYTES32_ABI, withSignerIfPossible)
+}
+
+export function useEIP2612Contract(tokenAddress?: string): Contract | null {
+  return useContract(tokenAddress, EIP_2612, false)
+}

d36de7e49276d3c5dbb7e63f7bfdf9f184f3741d
tunogya <tunogya@qq.com>
2021-09-29 14:04:13 +0800

    add Web3ReactManager

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,21 +4,24 @@ import { Stack } from "@chakra-ui/react"
 import Header from "../components/Header"
 import Wallet from "./Wallet"
 import Swap from "./Swap"
+import Web3ReactManager from "../components/Web3ReactManager"
 
 function App() {
   return (
-    <Stack spacing={0} minH={"100vh"}>
-      <Stack position={"fixed"} w={"100%"}>
-        <Header />
+    <Web3ReactManager>
+      <Stack spacing={0} minH={"100vh"}>
+        <Stack position={"fixed"} w={"100%"}>
+          <Header />
+        </Stack>
+        <Stack p={"120px 16px 0 16px"} alignItems={"center"}>
+          <Switch>
+            <Route exact strict path="/swap" component={Swap} />
+            <Route exact strict path="/" component={Wallet} />
+            <Route />
+          </Switch>
+        </Stack>
       </Stack>
-      <Stack p={"120px 16px 0 16px"} alignItems={"center"}>
-        <Switch>
-          <Route exact strict path="/swap" component={Swap} />
-          <Route exact strict path="/" component={Wallet} />
-          <Route />
-        </Switch>
-      </Stack>
-    </Stack>
+    </Web3ReactManager>
   )
 }
 

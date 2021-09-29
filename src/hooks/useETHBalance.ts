302fcee14e35faad9ff84f254503a1d26b5de26b
tunogya <tunogya@qq.com>
2021-09-29 18:32:34 +0800

    add atom

--- src/hooks/useETHBalance.ts
+++ src/hooks/useETHBalance.ts
@@ -1,17 +1,22 @@
 import {useActiveWeb3React} from "./web3"
 import {isAddress} from "../utils"
-import { ethers } from 'ethers'
-import {useState} from "react";
+import {ethers} from 'ethers'
+import {atom, useRecoilState} from "recoil";
+
+const ethBalanceAtom = atom({
+  key: "eth-balance",
+  default: ""
+})
 
 export const useETHBalance = (uncheckedAddresses: string | undefined) => {
-  const { library } = useActiveWeb3React()
-  const [balance, setBalance] = useState("")
+  const {library} = useActiveWeb3React()
+  const [balance, setBalance] = useRecoilState(ethBalanceAtom)
 
   if (!uncheckedAddresses || !isAddress(uncheckedAddresses)) {
     return undefined
   }
 
-  library?.getBalance(uncheckedAddresses).then((balance)=> {
+  library?.getBalance(uncheckedAddresses).then((balance) => {
       setBalance(ethers.utils.formatEther(balance))
     }
   )

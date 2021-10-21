c7ff098716e4b6e234bd027e663a104ce433e52b
tunogya <tunogya@qq.com>
2021-10-21 13:57:27 +0800

    add callStatic method

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,12 +1,21 @@
 import {HStack, Input, VStack, Text, Button} from "@chakra-ui/react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useSumContract} from "../../hooks/useContract";
 
 export const Swap = () => {
   const [curVal, setCurVal] = useState(0)
   const [testVal, setTestVal] = useState(0)
   const [a, setA] = useState("")
   const [b, setB] = useState("")
-  console.log(a, b)
+  const useSum = useSumContract("0x0a0c5dd69d2658fb0da4a180e24e190ceaf6b5e2")
+
+  useEffect(() => {
+    if (Number(a) < 0 || Number(b) < 0) {setTestVal(0); return}
+    useSum?.callStatic.quoteExactInput(Number(a), Number(b)).then(res => {
+      setTestVal(res.toString())
+    })
+  }, [a, b, setTestVal])
+
   return (
     <VStack alignItems={"flex-start"} w="400px" spacing={8}>
       <Text>当前合约值:{curVal}。</Text>
@@ -21,7 +30,6 @@ export const Swap = () => {
         </HStack>
       </VStack>
       <HStack w={"100%"}>
-        <Button isFullWidth variant={"outline"}>预计结果</Button>
         <Button isFullWidth>提交区块</Button>
       </HStack>
       <Text>预计合约值增加 {testVal}</Text>

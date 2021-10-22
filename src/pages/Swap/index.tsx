6f8029560b8223958e79df4513108314d0504fa5
tunogya <tunogya@qq.com>
2021-10-22 18:33:30 +0800

    fixed unit

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,18 +1,19 @@
 import {HStack, Input, VStack, Text, Button} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
 import {useSumContract} from "../../hooks/useContract";
+import { ethers } from "ethers"
 
 export const Swap = () => {
   const [curVal, setCurVal] = useState(0)
-  const [testVal, setTestVal] = useState(0)
-  const [a, setA] = useState("")
-  const [b, setB] = useState("")
+  const [testVal, setTestVal] = useState("")
+  const [a, setA] = useState("0")
+  const [b, setB] = useState("0")
   const useSum = useSumContract("0x0a0c5dd69d2658fb0da4a180e24e190ceaf6b5e2")
 
   useEffect(() => {
-    if (Number(a) < 0 || Number(b) < 0) {setTestVal(0); return}
-    useSum?.callStatic.quoteExactInput(Number(a), Number(b)).then(res => {
-      setTestVal(res.toString())
+    if (ethers.utils.parseEther(a || "0").lt(0) ||ethers.utils.parseEther(b || "0").lt(0)) {setTestVal("0"); return}
+    useSum?.callStatic.quoteExactInput(ethers.utils.parseEther(a || "0"), ethers.utils.parseEther(b || "0")).then(res => {
+      setTestVal(ethers.utils.formatEther(res))
     })
   }, [a, b, setTestVal])
 

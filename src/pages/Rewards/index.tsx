9e3b7144bdfdc60a6ca966b7f857570e952f4fad
tunogya <tunogya@qq.com>
2021-12-15 22:41:56 +0800

    fetch balance

--- src/pages/Rewards/index.tsx
+++ src/pages/Rewards/index.tsx
@@ -1,8 +1,16 @@
 import { Button, Spacer, Stack, Text } from "@chakra-ui/react"
 import { useActiveWeb3React } from "../../hooks/web3"
+import useToken from "../../hooks/useToken";
+import {YAKYAK_REWARDS_ADDRESS} from "../../constants/addresses";
+import {useEffect} from "react";
 
 export const Rewards = () => {
-  const { account } = useActiveWeb3React()
+  const { account, chainId } = useActiveWeb3React()
+  const token = useToken(YAKYAK_REWARDS_ADDRESS[chainId ?? 4])
+
+  useEffect(()=>{
+    token.fetchBalance(account)
+  }, [account, token])
 
   return (
     <Stack w={"container.md"} h={"full"} spacing={16}>
@@ -16,7 +24,7 @@ export const Rewards = () => {
         <Stack direction={"row"} alignItems={"center"}>
           <Stack direction={"row"} alignItems={"baseline"}>
             <Text fontSize={"3xl"} fontWeight={"bold"}>
-              322.2
+              { token.balance ? token.balance : "NaN" }
             </Text>
             <Text fontSize={"xl"} fontWeight={"bold"} fontStyle={"italic"}>
               YakYak©

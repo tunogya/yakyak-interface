9b437388aa9e11657c5ef3e0c7e96366f04e9c57
tunogya <tunogya@qq.com>
2021-12-19 15:54:50 +0800

    update

--- src/pages/Bank/Exchange.tsx
+++ src/pages/Bank/Exchange.tsx
@@ -4,10 +4,17 @@ import React from "react";
 const Exchange = () => {
   return (
     <Stack bg={"blue.300"} w={"400px"} h={"600px"} p={[2, 4, 4, 8]} borderRadius={"20px"} spacing={[2, 4, 4, 8]}>
-      <Stack direction={"row"}>
-        <Text color={"blue.700"} fontWeight={"bold"}>My bank:</Text>
-        <Spacer />
-        <Text fontWeight={"bold"} color={"blue.700"}>200000000 YakYak®</Text>
+      <Stack>
+        <Stack direction={"row"}>
+          <Text color={"blue.700"} fontWeight={"bold"}>My wallet:</Text>
+          <Spacer />
+          <Text fontWeight={"bold"} color={"blue.700"}>200000000 YakYak®</Text>
+        </Stack>
+        <Stack direction={"row"}>
+          <Text color={"blue.700"} fontWeight={"bold"}>My bank:</Text>
+          <Spacer />
+          <Text fontWeight={"bold"} color={"blue.700"}>0 YakYak®</Text>
+        </Stack>
       </Stack>
       <Heading fontSize={"xl"}>Exchange Cheque</Heading>
 

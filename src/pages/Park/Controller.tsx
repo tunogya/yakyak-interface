bf208bc1a8ecb4da8b27d6a468defb2cb9e65e7c
tunogya <tunogya@qq.com>
2022-02-07 23:46:30 +0800

    fixed

--- src/pages/Park/Controller.tsx
+++ src/pages/Park/Controller.tsx
@@ -1,6 +1,5 @@
 import {Stack, Text} from "@chakra-ui/react";
 import {FC, ReactElement, useState} from "react";
-import Yaklon from "./Yaklon";
 import {useNavigate, useParams} from "react-router-dom";
 
 type ControllerProps = {
@@ -12,8 +11,8 @@ export const Controller: FC<ControllerProps> = ({...props}) => {
   const navigator = useNavigate()
   const params = useParams()
   const menu = [
-    {label: 'Yaklon', link: 'yaklon'},
-    {label: 'Adopt', link: 'adopt'},
+    {label: 'YAKLON', link: 'yaklon'},
+    {label: 'ADOPT', link: 'adopt'},
   ]
 
   return (
@@ -25,7 +24,7 @@ export const Controller: FC<ControllerProps> = ({...props}) => {
         {props.tool}
       </Stack>
       <Stack w={"full"} maxW={"1064px"} position={"absolute"} top={"150px"} hidden={hiddenMenu}>
-        <Stack bg={"white"} w={"240px"} p={"20px"} boxShadow={"xs"} borderRadius={8} fontSize={"14px"} onMouseLeave={()=> setHiddenMenu(true)}>
+        <Stack bg={"white"} spacing={'20px'} w={"240px"} p={"20px"} boxShadow={"xs"} borderRadius={8} fontSize={"14px"} onMouseLeave={()=> setHiddenMenu(true)}>
           { menu.map((item)=> (
             <Text fontWeight={"600"} cursor={"pointer"} _hover={{ color: "primary" }}  onClick={()=>{
               navigator(`/shopping/${item.link}`)
